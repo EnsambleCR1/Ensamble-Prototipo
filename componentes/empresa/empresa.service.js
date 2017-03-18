a3bf0e050a8301f0899d808165e61c8770adc676
Luisk955 <ldelvalleu@ucenfotec.ac.cr>
2017-03-18 16:36:04 -0600

    empresafuncinal1

--- componentes/empresa/empresa.service.js
+++ componentes/empresa/empresa.service.js
@@ -4,18 +4,19 @@
   .service('empresaService', empresaService);
 
   function empresaService(){
-    var empresa = [];
+    var empresas = [];
     var publicAPI = {
-      setEmpresa : _setEmpresa,
-      getEmpresa : _getEmpresa
+      setEmpresas : _setEmpresas,
+      getEmpresas : _getEmpresas
     };
     return publicAPI; // todas las funciones que sean llamadas por ajax deben estar debajo del return, para que cuando angular corra el script haga el return y devuelva el api , las funciones debajo del return son privadas y se devuelve el api que es el que contiene las funciones
 
-    function _setEmpresa(){
-      return empresa;
+    function _setEmpresas(pEmpresa){
+      empresas.push(pEmpresa);
+      alert('Empresa Guardada');
     }
 
-    function _getEmpresa(){
+    function _getEmpresas(){
       return empresa;
     }
   }
